7818bf23d67102b7bc96dbcd8bc9350fddc8e5c8
Sean Zhou <seantxzhou12@gmail.com>
2020-05-02 14:23:07 -0400

    add new route

--- flaskr/backlog.py
+++ flaskr/backlog.py
@@ -26,13 +26,57 @@ def add_type():
                 'INSERT INTO backlog_types (backlog_type) VALUES (?)', (type_name,)
             )
             db.commit()
-            return 'Success!'
+            return jsonify(msg='Success!')
     elif request.method == 'GET':
         if type_in_db is None:
             error = 'Type name {} does not exist'.format(type_name)
         if error is None:
             print(type_in_db['id'])
-            return jsonify(str(type_in_db['id']))
+            return jsonify(id=str(type_in_db['id']))
     return error
     # flash(error)
 
+def convert_backlog_row_to_json(row):
+    return jsonify(
+        id=row['id'],
+        backlog_type=row['backlog_type'],
+        task_name=row['task_name'],
+        created=row['created'],
+        goal_date=row['goal_date']
+    )
+
+@bp.route('/item', methods=('GET', 'POST'))
+def item():
+    db = get_db()
+    if request.method == 'POST':
+        type_id = request.form['type_id']
+        task_name = request.form['name']
+        goal_date = None
+        # goal_date = request.form['date']
+        if not type_id:
+            return jsonify(error='No type provided')
+        elif not task_name:
+            return jsonify(error='No task name provided')
+        if not goal_date:
+            db.execute(
+                'INSERT INTO backlog (backlog_type, task_name) VALUES (?, ?)', 
+                (type_id, task_name,)
+            )
+        else:
+            db.execute(
+                'INSERT INTO backlog (backlog_type, task_name, goal_date) VALUES (?, ?, ?)', 
+                (type_id, type_name, goal_date)
+            )
+        db.commit()
+        return jsonify(msg='Success!')
+    # elif request.method == 'GET':
+    else:
+        id = request.form['backlog_id']
+        if not id:
+            return jsonify(error='No ID provided')
+        row = db.execute(
+            'SELECT * FROM backlog WHERE id = ?', (id,)
+        ).fetchone()
+        if row is None:
+            return jsonify(error='No backlog matches that id!')
+        return convert_backlog_row_to_json(row)
