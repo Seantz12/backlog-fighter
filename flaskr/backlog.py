af958b89d6228709f82df8fda6b95a374ba429f2
Sean Zhou <seantxzhou12@gmail.com>
2020-05-09 13:39:30 -0400

    refactor add type handler

--- flaskr/backlog.py
+++ flaskr/backlog.py
@@ -8,33 +8,36 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/backlog')
 
+def add_type(form, db):
+    type_name = form['type']
+    type_in_db = db.execute(
+        'SELECT id FROM backlog_types WHERE backlog_type = ?', (type_name,)
+    ).fetchone()
+    if not type_name:
+        return jsonify(error='Type name is required!')
+    if type_in_db is not None:
+        return jsonify(error='Type name {} is already defined.'.format(type_name))
+    db.execute(
+        'INSERT INTO backlog_types (backlog_type) VALUES (?)', (type_name,)
+    )
+    db.commit()
+    return jsonify(msg='Success!')
+
+def get_all_types(db):
+    all_types = db.execute(
+        'SELECT * FROM backlog_types'
+    ).fetchall()
+    all_type_names = [row['backlog_type'] for row in all_types]
+    return jsonify(types=all_type_names)
+
+
 @bp.route('/type', methods=('GET', 'POST'))
-def add_type():
+def type_handler():
     db = get_db()
-    error = None
     if request.method == 'POST':
-        type_name = request.form['type']
-        type_in_db = db.execute(
-            'SELECT id FROM backlog_types WHERE backlog_type = ?', (type_name,)
-        ).fetchone()
-        if not type_name:
-            error = 'Type name is required'
-        if type_in_db is not None:
-            error = 'Type name {} is already defined'.format(type_name)
-        if error is None:
-            db.execute(
-                'INSERT INTO backlog_types (backlog_type) VALUES (?)', (type_name,)
-            )
-            db.commit()
-            return jsonify(msg='Success!')
+        return add_type(request.form, db)
     elif request.method == 'GET':
-        if error is None:
-            all_types = db.execute(
-                'SELECT * FROM backlog_types'
-            ).fetchall()
-            all_type_names = [row['backlog_type'] for row in all_types]
-            return jsonify(types=all_type_names)
-    return error
+        return get_all_types(db)
 
 def convert_backlog_row_to_json(row):
     return {
@@ -46,7 +49,7 @@ def convert_backlog_row_to_json(row):
     }
 
 @bp.route('/item', methods=('GET', 'POST'))
-def item():
+def item_handler():
     db = get_db()
     if request.method == 'POST':
         type_id = request.form['type_id']
